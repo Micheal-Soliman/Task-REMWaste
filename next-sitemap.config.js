8c092a12c39098f58646870f0b7968b9b08b7468
Micheal-Soliman <michealsoliman2@gmail.com>
2025-06-26 22:41:01 +0300

    add file4

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://task-rem-waste-oeki.vercel.app', // ← غيّر ده للرابط الحقيقي بتاعك
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 5000,
+};
